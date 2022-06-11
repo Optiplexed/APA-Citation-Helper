a874f194616fa2f1e34da4a95f75d166d7ddad43
Optiplexed <studyperson1@gmail.com>
2022-06-11 12:21:48 -0400

    Alpha Version

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 textFieldList = []
 textFieldMap = new Map();
+var lastCitation = null;
 
 function createTextField(nameType, styles = [])
    {
@@ -35,7 +36,7 @@ function createAuthorDiv()
    parentDiv.appendChild(createTextField("First Name"));
 
    var middleName = createTextField("Middle Name");
-   middleName.classList.add("pad-right-left-1");
+   middleName.classList.add("pad-right-left");
    parentDiv.appendChild(middleName);
 
    parentDiv.appendChild(createTextField("Last Name"));
@@ -48,13 +49,13 @@ function createDateDiv()
    var parentDiv = document.createElement("div");
    parentDiv.classList.add("side");
    parentDiv.classList.add("pad-top-bottom");
-   parentDiv.appendChild(createTextField("Year"));
+   parentDiv.appendChild(createTextField("Month"));
 
-   var month = createTextField("Month");
-   month.classList.add("pad-right-left-1");
+   var month = createTextField("Day");
+   month.classList.add("pad-right-left");
    parentDiv.appendChild(month);
 
-   parentDiv.appendChild(createTextField("Day"));
+   parentDiv.appendChild(createTextField("Year"));
 
    return wrapAndTitle(parentDiv, "Date");
    }
@@ -88,6 +89,75 @@ function wrapAndTitle(targetDiv, title)
 
    return parentDiv;
    }
+
+function isInvalid(str)
+   {
+   formatError = true;
+   return typeof(str) === "undefined" || str === null || str === "";
+   }
+function getAndResetFormatError()
+   {
+   var toReturn = formatError;   
+   formatError = false;
+   return toReturn;
+   }
+function getField(fieldName, modify = null)
+   {
+   var returnVal = textFieldMap.get(fieldName).value;   
+   if(!isInvalid(returnVal))
+      {
+      return modify === null ? returnVal : modify(returnVal);   
+      }
+   return "";
+   }
+function numToDate(num)
+   {
+   switch(num)
+      {
+      case 1: return "January";
+      case 2: return "February";
+      case 3: return "March";
+      case 4: return "April";
+      case 5: return "May";
+      case 6: return "June"; 
+      case 7: return "July";
+      case 8: return "August";
+      case 9: return "September";
+      case 10: return "October";
+      case 11: return "November";
+      case 12: return "December";
+      default:
+      return null;
+      }   
+   }
+function isValidDate(date)
+   {
+   parsed = parseInt(date)
+   return !isNaN(parsed) && parsed >= 1  && parsed <= 12;
+   }
+function formatCitation()
+   {
+   var formatted = "";
+   formatError = false;
+   
+   var name = getField("last-name", (str) => str[0].toLocaleUpperCase() + str.slice(1) + ", ");
+   name += getField("first-name", (str) => str[0].toLocaleUpperCase() + ".");
+   name += getField("middle-name", (str) => " " + str[0].toLocaleUpperCase() + ".");
+
+   var date = getField("year", (str) => str += ", ");
+   date += getField("month", (str) => str += " ");
+   date += getField("day", (str) => isValidDate(str) ? numToDate(parseInt(str)) : str);
+
+   var title = getField("title", (str) => str += ".");
+   var publisher = getField("publisher", (str) => str += ".");
+   var url = getField("url");
+
+   formatted = `${name} (${date}) <em>${title}</em> ${publisher} ${url}`;
+
+
+   //Aubrey, A. (2019, December 12) How to reduce food waste. NPR. https://www.npr.org/2019/12/10/786867315/how-to-reduce-food-waste
+   return formatted;
+   }
 function createCitation()
    {
    var citationText = document.createElement("p");
@@ -95,14 +165,8 @@ function createCitation()
 
    console.log(textFieldMap.get("first-name"));
 
-   var formattedText = "";
-   for(var i = 0; i < textFieldList.length; i++)
-      {
-      formattedText += (textFieldList[i]) + "\n";   
-      } 
-   citationText.innerText = formattedText;
+   citationText.innerHTML = formatCitation();
    
-
    var parentDiv = document.createElement("div");
    parentDiv.classList.add("citation-card");
    
@@ -112,33 +176,63 @@ function createCitation()
    }
 function onSubmitClick()
    {
-   document.body.appendChild(createCitation());
+   var newCitation = createCitation();
+   document.body.appendChild(newCitation);
+   if(lastCitation !== null)
+      {
+      document.body.insertBefore(newCitation, lastCitation);
+      }
+   lastCitation = newCitation;
    }
 
-function createSubmitButton()
+function onClearClick()
+   {
+   for(var i = 0; i < textFieldList.length; i++)
+      {
+      textFieldMap.get(textFieldList[i]).value = "";   
+      }   
+   }
+
+function createButtons()
    {
    var parentDiv = document.createElement("div"); 
-   var button = document.createElement("button");  
+   var submitButton = document.createElement("button"); 
+   var clearButton = document.createElement("button"); 
+   var spacing = document.createElement("div");
    
-   button.classList.add("submit-button");
-   button.type = "submit";
-   button.onclick = onSubmitClick;
+   submitButton.classList.add("button");
+   submitButton.type = "submit";
+   submitButton.onclick = onSubmitClick;
 
    var submitText = document.createElement("span");  
    submitText.innerText = "Submit";
-   submitText.classList.add("submit-text");
+   submitText.classList.add("button-text");
+
+   clearButton.classList.add("button");
+   clearButton.type = "submit";
+   clearButton.onclick = onClearClick;
+
+   var clearText = document.createElement("span");  
+   clearText.innerText = "Clear";
+   clearText.classList.add("button-text");
+
+   spacing.classList.add("pad-right");
    
-   //parentDiv.classList.add("Center")
+   submitButton.appendChild(submitText);
+   clearButton.appendChild(clearText);
 
-   button.appendChild(submitText);
-   parentDiv.appendChild(button);
+   parentDiv.appendChild(submitButton);
+   parentDiv.appendChild(spacing);
+   parentDiv.appendChild(clearButton);
 
-   return button;
+   parentDiv.classList.add("side");
+
+   return parentDiv;
    }
 
 function initDocument()
    {
-   var cardElement = document.getElementsByClassName("card")[0];
+   cardElement = document.getElementsByClassName("card")[0];
    
    elements = [createAuthorDiv(), createDateDiv(), createOtherDiv()];
    elements.forEach(element => {
@@ -146,7 +240,7 @@ function initDocument()
       cardElement.appendChild(element);
       });
 
-   cardElement.appendChild(createSubmitButton());
+   cardElement.appendChild(createButtons());
    }
 
 document.addEventListener('DOMContentLoaded', function() {
