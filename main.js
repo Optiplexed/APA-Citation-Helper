93d6b7b0b94da38a6a0f17d502384ba6edabadf4
millersd25 <millersd25@vt.edu>
2022-06-11 08:23:25 -0400

    Almost Done

--- main.js
+++ main.js
@@ -2,18 +2,13 @@ function createTextField(nameType)
    {
    var parentDiv = document.createElement("div");
 
-   var title = document.createElement("span");
-   title.innerHTML = nameType;
-   title.classList.add("small-padding");
-
    var textField = document.createElement("input");
    textField.type = "text";
    textField.name = nameType.replace(" ", "-");
-   textField.placeholder = title.innerHTML
+   textField.placeholder = nameType;
    textField.maxlength = 150
    textField.classList.add("text-input")
 
-   parentDiv.appendChild(title);
    parentDiv.appendChild(textField);
    parentDiv.classList.add("name-input-pair");
 
@@ -24,27 +19,81 @@ function createAuthorDiv()
    {
    var parentDiv = document.createElement("div");
    parentDiv.classList.add("side");
-   parentDiv.classList.add("card-element");
+   parentDiv.classList.add("pad-top-bottom");
+
    parentDiv.appendChild(createTextField("First Name"));
-   parentDiv.appendChild(createTextField("Middle Name"));
+
+   var middleName = createTextField("Middle Name");
+   middleName.classList.add("pad-right-left-1");
+   parentDiv.appendChild(middleName);
+
    parentDiv.appendChild(createTextField("Last Name"));
 
+   return wrapAndTitle(parentDiv, "Author");
+   }
+
+function createDateDiv()
+   {
+   var parentDiv = document.createElement("div");
+   parentDiv.classList.add("side");
+   parentDiv.classList.add("pad-top-bottom");
+   parentDiv.appendChild(createTextField("Year"));
+
+   var month = createTextField("Month");
+   month.classList.add("pad-right-left-1");
+   parentDiv.appendChild(month);
+
+   parentDiv.appendChild(createTextField("Day"));
+
+   return wrapAndTitle(parentDiv, "Date");
+   }
+
+function createOtherDiv()
+   {
+   var parentDiv = document.createElement("div");
+   parentDiv.classList.add("side");
+   parentDiv.classList.add("pad-top-bottom");
+   parentDiv.appendChild(createTextField("Title"));
+
+   var month = createTextField("URL");
+   month.classList.add("pad-right-left-1");
+   parentDiv.appendChild(month);
+
+   parentDiv.appendChild(createTextField("Publisher"));
+
+   return wrapAndTitle(parentDiv, "Other");
+   }
+
+function wrapAndTitle(targetDiv, title)
+   {
+   var parentDiv = document.createElement("div");   
+
+   var subTitle = document.createElement("p");
+   subTitle.classList.add("sub-title");
+   subTitle.innerHTML = title;
+
+   parentDiv.appendChild(subTitle);
+   parentDiv.appendChild(targetDiv);
+
    return parentDiv;
+   }
+
+function createSubmitButton()
+   {
+   var buttonDiv = document.createElement("div");  
+   
+   return buttonDiv;
    }
 
 function initDocument()
    {
    var cardElement = document.getElementsByClassName("card")[0];
-   textTypes = ["First Name", "Last Name", "Date"];
    
-   var formatType = document.createElement("span");
-   formatType.innerHTML = "Website";
-   formatType.classList.add("sub-title");
-
-   cardElement.appendChild(formatType);
-   cardElement.appendChild(createAuthorDiv());
-   cardElement.appendChild(createTextField("title"));
-   cardElement.appendChild(createTextField("URL"));
+   elements = [createAuthorDiv(), createDateDiv(), createOtherDiv()];
+   elements.forEach(element => {
+      element.classList.add("pad-bottom");
+      cardElement.appendChild(element);
+      });
    }
 
 document.addEventListener('DOMContentLoaded', function() {
